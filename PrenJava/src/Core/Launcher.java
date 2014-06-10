9f5277fe75a366f0dafbc576aa69834e7acf0d57
tdelmige <sandro.elmiger@stud.hslu.ch>
2014-06-10 22:03:07 +0200

    Log4J2 hinzugefügt, noch nich lauffähig da xml config noch fehlt und die Logging aufrufe.

--- PrenJava/src/Core/Launcher.java
+++ PrenJava/src/Core/Launcher.java
@@ -3,6 +3,8 @@ package Core;
 import Common.Color;
 import ImageProcessing.FilterSet;
 import ImageProcessing.PropertyManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.opencv.core.Scalar;
 import org.opencv.core.Core;
 
@@ -10,10 +12,14 @@ import java.io.File;
 import java.nio.ByteBuffer;
 
 public class Launcher {
-	
+
+    private static Logger log = LogManager.getLogger(Launcher.class.getName());
 
 	public static void main(String arg[])
 	{
+        log.debug("Test Debug info");
+
+
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         //System.load(new File("PrenJava/res/OpenCV/libopencv_java248.dylib").getAbsolutePath());
 
